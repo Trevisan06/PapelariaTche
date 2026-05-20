70fa579b86611478fbc237fc3288b0988a7c3d0e
Trevisan06 <lucasptrevissan@gmail.com>
2026-05-20 17:21:31 -0300

    Update exibir_saldo_pontos.py

--- Control/exipicao/exibir_saldo_pontos.py
+++ Control/exipicao/exibir_saldo_pontos.py
@@ -13,13 +13,13 @@ def exibir_dados_saldo_pontos():
     if conexao:
         cursor = conexao.cursor()
 
-        cursor.execute("SELECT * FROM saldoPontos")
+        cursor.execute("SELECT id_pontos, saldo_atual, pontos_ganhos, pontos_usados, data_movimentacao, status_conta  FROM saldoPontos")
         resultados = cursor.fetchall()
 
         print("\nSaldo Pontos:")
 
-        for id_pontos, cpf_cliente, saldo_atual, pontos_ganhos, pontos_usados, data_movimentacao, status_conta in resultados:
-            print(f"ID: {id_pontos} | CPF Cliente: {cpf_cliente} | Saldo atual: {saldo_atual} | Pontos ganhos: {pontos_ganhos} | Pontos usados: {pontos_usados} | Última movimentação: {data_movimentacao} | Status conta: {status_conta}")
+        for id_pontos, saldo_atual, pontos_ganhos, pontos_usados, data_movimentacao, status_conta in resultados:
+            print(f"ID: {id_pontos} |  Saldo atual: {saldo_atual} | Pontos ganhos: {pontos_ganhos} | Pontos usados: {pontos_usados} | Última movimentação: {data_movimentacao} | Status conta: {status_conta}")
 
         cursor.close()
         conexao.close()
